9766c7a0bdc55c4da09c693bcaee0093170f3535
injflea727 <107214394+injflea727@users.noreply.github.com>
2023-05-08 20:22:42 +0000

    modified:   main.js 	modified:   package.json

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.136/build/three.module.js';
 
 import {FirstPersonControls} from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FirstPersonControls.js';
 
@@ -13,14 +13,15 @@ function clamp(x, a, b) {
   return Math.min(Math.max(x, a), b);
 }
 
-class InputController{
+class InputController {
   constructor() {
-    this.Initialize_();
+    this.initialize_();    
   }
-  Initialize_(){
-this.current_ = {
-      leftbutton: false,
-      rightbutton: false,
+
+  initialize_() {
+    this.current_ = {
+      leftButton: false,
+      rightButton: false,
       mouseXDelta: 0,
       mouseYDelta: 0,
       mouseX: 0,
@@ -29,97 +30,113 @@ this.current_ = {
     this.previous_ = null;
     this.keys_ = {};
     this.previousKeys_ = {};
-
-    document.addEventListener('mousedown',(e) => this.onMouseDown_(e),false);
-    document.addEventListener('mouseup',(e) => this.onMouseUp_(e),false);
-    document.addEventListener('mousemove',(e) => this.onMouseMove_(e),false);
-    document.addEventListener('keydown',(e) => this.onKeyDown_(e),false);
-    document.addEventListener('keyup',(e) => this.onKeyUp_(e),false);
-  }
-
-    onMouseDown_(e){
-    switch(e.button){
-      case 0: {
-        this.current_.leftbutton = true;
-        break;
-      }
-      case 2: {
-        this.current_.rightbutton = true;
-        break;
-      }
-    }
-  }
-
-  onMouseUp_(e){
-    switch(e.button){
-      case 0: {
-        this.current_.leftbutton = false;
-        break;
-      }
-      case 2: {
-        this.current_.rightbutton = false;
-        break;
-      }
-    }
+    document.addEventListener('mousedown', (e) => this.onMouseDown_(e), false);
+    document.addEventListener('mousemove', (e) => this.onMouseMove_(e), false);
+    document.addEventListener('mouseup', (e) => this.onMouseUp_(e), false);
+    document.addEventListener('keydown', (e) => this.onKeyDown_(e), false);
+    document.addEventListener('keyup', (e) => this.onKeyUp_(e), false);
   }
 
   onMouseMove_(e) {
-  this.current_.mouseX = e.pageX - window.innerWidth/2;
-  this.current_.mouseY = e.pageY - window.innerHeight/2;
+    this.current_.mouseX = e.pageX - window.innerWidth / 2;
+    this.current_.mouseY = e.pageY - window.innerHeight / 2;
 
-  if (this.previous_ === null) {
-      this.previous_ ={...this.current_};
+    if (this.previous_ === null) {
+      this.previous_ = {...this.current_};
     }
 
     this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
     this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
   }
 
+  onMouseDown_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = true;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = true;
+        break;
+      }
+    }
+  }
+
+  onMouseUp_(e) {
+    this.onMouseMove_(e);
+
+    switch (e.button) {
+      case 0: {
+        this.current_.leftButton = false;
+        break;
+      }
+      case 2: {
+        this.current_.rightButton = false;
+        break;
+      }
+    }
+  }
+
   onKeyDown_(e) {
     this.keys_[e.keyCode] = true;
   }
 
-  onKeyUp_ (e) {
+  onKeyUp_(e) {
     this.keys_[e.keyCode] = false;
   }
 
-  update_() {
-    this.previous_ = {...this.current_};
+  key(keyCode) {
+    return !!this.keys_[keyCode];
   }
-}
 
-class FirstPersonCamera{
-  constructor(camera){
+  update(_) {
+    if (this.previous_ !== null) {
+      this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+      this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+
+      this.previous_ = {...this.current_};
+    }
+  }
+};
+
+class FirstPersonCamera {
+  constructor(camera) {
     this.camera_ = camera;
     this.input_ = new InputController();
     this.rotation_ = new THREE.Quaternion();
-    this.translation_ = new THREE.Vector3();
+    this.translation_ = new THREE.Vector3(0, 2, 0);
     this.phi_ = 0;
-
+    this.phiSpeed_ = 8;
     this.theta_ = 0;
+    this.thetaSpeed_ = 5;
   }
 
-  update_(timeElapsedS) {
-    this.updadeRotation_(timeElapsedS);
+  update(timeElapsedS) {
+    this.updateRotation_(timeElapsedS);
     this.updateCamera_(timeElapsedS);
+    this.updateTranslation_(timeElapsedS);
+    this.input_.update(timeElapsedS);
   }
 
   updateCamera_(_) {
     this.camera_.quaternion.copy(this.rotation_);
+    this.camera_.position.copy(this.translation_);
   }
 
   updateTranslation_(timeElapsedS){
-    const forwardVelocity =  (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0);
-    const strafeVelocity =  (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0);
+    const forwardVelocity = (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0)
+    const strafeVelocity = (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0)
 
     const qx = new THREE.Quaternion();
-    qx.setFromAxisAngle(new THREE.Vector3(0,1,0), this.phi_);
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
 
-    const forward = new THREE.Vector3(0,0,-1);
+    const forward = new THREE.Vector3(0, 0, -1);
     forward.applyQuaternion(qx);
     forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
 
-    const left = new THREE.Vector3(-1,0,0);
+    const left = new THREE.Vector3(-1, 0, 0);
     left.applyQuaternion(qx);
     left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
 
@@ -127,59 +144,67 @@ class FirstPersonCamera{
     this.translation_.add(left);
   }
 
-  updadeRotation_(timeElapsedS){
+  updateRotation_(timeElapsedS) {
     const xh = this.input_.current_.mouseXDelta / window.innerWidth;
     const yh = this.input_.current_.mouseYDelta / window.innerHeight;
-    this.phi_ += -xh * 5; 
-    this.theta_ = clamp(this.theta_ + -yh * 5, Math.PI / 3, Math.PI / 3);
+
+    this.phi_ += -xh * this.phiSpeed_;
+    this.theta_ = clamp(this.theta_ + -yh * this.thetaSpeed_, -Math.PI / 3, Math.PI / 3);
 
     const qx = new THREE.Quaternion();
-    qx.setFromAxisAngle(new THREE.Vector3(0,1,0), this.phi_);
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
     const qz = new THREE.Quaternion();
-    qz.setFromAxisAngle(new THREE.Vector3(1,0,0), this.theta_);
+    qz.setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.theta_);
 
     const q = new THREE.Quaternion();
     q.multiply(qx);
     q.multiply(qz);
-    
+
     this.rotation_.copy(q);
   }
 }
 
-class CharacterControllerDemo {
+class FirstPersonCameraDemo {
   constructor() {
-    this._Initialize();
+    this.initialize_();
   }
-    
-  _Initialize() {
-    this.InitializeDemo_();
-    this.InitializeRender_();
-    this.InitializeLights_();
-    this.InitializeScene_();
 
-    this._previousRAF = null;
-    this._OnWindowResize();
-    this._RAF();
+  initialize_() {
+    this.initializeRenderer_();
+    this.initializeLights_();
+    this.initializeScene_();
+    this.initializePostFX_();
+    this.initializeDemo_();
+
+    this.previousRAF_ = null;
+    this.raf_();
+    this.onWindowResize_();
   }
-    InitializeDemo_() {
-      this.fpsCamera_ = new FirstPersonCamera(this.camera_);
-    }
 
-    InitializeRender_(){
-    this._threejs = new THREE.WebGLRenderer({
-      antialias: true,
+  initializeDemo_() {
+    // this.controls_ = new FirstPersonControls(
+    //     this.camera_, this.threejs_.domElement);
+    // this.controls_.lookSpeed = 0.8;
+    // this.controls_.movementSpeed = 5;
+
+    this.fpsCamera_ = new FirstPersonCamera(this.camera_);
+  }
+
+  initializeRenderer_() {
+    this.threejs_ = new THREE.WebGLRenderer({
+      antialias: false,
     });
-    
-    this._threejs.outputEncoding = THREE.sRGBEncoding;
-    this._threejs.shadowMap.enabled = true;
-    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
-    this._threejs.setPixelRatio(window.devicePixelRatio);
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.shadowMap.enabled = true;
+    this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.threejs_.setPixelRatio(window.devicePixelRatio);
+    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.physicallyCorrectLights = true;
+    this.threejs_.outputEncoding = THREE.sRGBEncoding;
 
-    document.body.appendChild(this._threejs.domElement);
+    document.body.appendChild(this.threejs_.domElement);
 
     window.addEventListener('resize', () => {
-      this._OnWindowResize();
+      this.onWindowResize_();
     }, false);
 
     const fov = 60;
@@ -187,38 +212,16 @@ class CharacterControllerDemo {
     const near = 1.0;
     const far = 1000.0;
     this.camera_ = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this.camera_.position.set(10, 2, 10);
+    this.camera_.position.set(0, 2, 0);
 
-    this._scene = new THREE.Scene();
+    this.scene_ = new THREE.Scene();
 
     this.uiCamera_ = new THREE.OrthographicCamera(
-      -1, 1, 1 * aspect, -1 * aspect, 1, 1000);
+        -1, 1, 1 * aspect, -1 * aspect, 1, 1000);
     this.uiScene_ = new THREE.Scene();
-    }
+  }
 
-    InitializeLights_() {
-    let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-    light.position.set(-100, 100, 100);
-    light.target.position.set(0, 0, 0);
-    light.castShadow = true;
-    light.shadow.bias = -0.001;
-    light.shadow.mapSize.width = 4096;
-    light.shadow.mapSize.height = 4096;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.left = 50;
-    light.shadow.camera.right = -50;
-    light.shadow.camera.top = 50;
-    light.shadow.camera.bottom = -50;
-    this._scene.add(light);
-    
-    light = new THREE.AmbientLight(0xFFFFFF, 0.25);
-    this._scene.add(light);
-    }
-
-    InitializeScene_(){
+  initializeScene_() {
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
         './resources/posx.jpg',
@@ -227,9 +230,10 @@ class CharacterControllerDemo {
         './resources/negy.jpg',
         './resources/posz.jpg',
         './resources/negz.jpg',
-    ]);
+  ]);
+
     texture.encoding = THREE.sRGBEncoding;
-    this._scene.background = texture;
+    this.scene_.background = texture;
 
     const plane = new THREE.Mesh(
         new THREE.PlaneGeometry(100, 100, 10, 10),
@@ -237,57 +241,97 @@ class CharacterControllerDemo {
     plane.castShadow = false;
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
-    this._scene.add(plane);
+    this.scene_.add(plane);
 
     const hotel = new THREE.Mesh(
-      new THREE.BoxGeometry(35,75,35),
-    new THREE.MeshStandardMaterial({color: 0x418782, }));
-
+      new THREE.BoxGeometry(35, 65, 35),
+      new THREE.MeshStandardMaterial({color: 0x808080,}));
     hotel.castShadow = false;
     hotel.receiveShadow = true;
-    hotel.position.set(40,37,0); 
-    this._scene.add(hotel);
+    hotel.position.set(40,30,0);
+    this.scene_.add(hotel);
 
-    const teleport = new THREE.Mesh(
-      new THREE.BoxGeometry(10,10,10),
-    new THREE.MeshStandardMaterial({color: 0x000000, }));
-    teleport.position.set(23,5,0); 
-    this._scene.add(teleport);
-    }
-
-  _OnWindowResize() {
-    this.camera_.aspect = window.innerWidth / window.innerHeight;
-    this.camera_.updateProjectionMatrix();
-    this._threejs.setSize(window.innerWidth, window.innerHeight);
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(5, 5, 5),
+      new THREE.MeshStandardMaterial({color: 0x437144,}));
+    box.castShadow = false;
+    box.receiveShadow = true;
+    box.position.set(10,2.5,0);
+    this.scene_.add(box);
   }
 
-  _RAF() {
+  initializeLights_() {
+    const distance = 500.0;
+    const angle = Math.PI / 4.0;
+    const penumbra = 0.5;
+    const decay = 1.0;
+
+    let random = new THREE.SpotLight();
+    random.castShadow = true
+    let light = new THREE.SpotLight(
+        0xFFFFFF, 100.0, distance, angle, penumbra, decay);
+    light.castShadow = true;
+    light.shadow.bias = -0.00001;
+    light.shadow.mapSize.width = 4096;
+    light.shadow.mapSize.height = 4096;
+    light.shadow.camera.near = 1;
+    light.shadow.camera.far = 100;
+
+    light.position.set(0, 25, 0);
+    light.lookAt(0, 0, 0);
+    this.scene_.add(light);
+
+    const upColour = 0xFFFF80;
+    const downColour = 0x808080;
+    light = new THREE.HemisphereLight(upColour, downColour, 0.5);
+    light.color.setHSL( 0.6, 1, 0.6 );
+    light.groundColor.setHSL( 0.095, 1, 0.75 );
+    light.position.set(0, 4, 0);
+    this.scene_.add(light);
+  }
+
+  initializePostFX_() {
+  }
+
+  onWindowResize_() {
+    this.camera_.aspect = window.innerWidth / window.innerHeight;
+    this.camera_.updateProjectionMatrix();
+
+    this.uiCamera_.left = -this.camera_.aspect;
+    this.uiCamera_.right = this.camera_.aspect;
+    this.uiCamera_.updateProjectionMatrix();
+
+    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  raf_() {
     requestAnimationFrame((t) => {
-      if (this._previousRAF === null) {
-        this._previousRAF = t;
+      if (this.previousRAF_ === null) {
+        this.previousRAF_ = t;
       }
-      this._Step(t - this._previousRAF);
+
+      this.step_(t - this.previousRAF_);
       this.threejs_.autoClear = true;
-      this._threejs.render(this._scene, this.camera_);
+      this.threejs_.render(this.scene_, this.camera_);
       this.threejs_.autoClear = false;
-      this._threejs.render(this.uiScene_, this.uiCamera_);
-      this._previousRAF = t;
-      this._RAF();
+      this.threejs_.render(this.uiScene_, this.uiCamera_);
+      this.previousRAF_ = t;
+      this.raf_();
     });
   }
 
-  _Step(timeElapsed) {
+  step_(timeElapsed) {
     const timeElapsedS = timeElapsed * 0.001;
 
-      //this._controls.Update(timeElapsedS);
-      this.fpsCamera_.Update(timeElapsedS);
+    // this.controls_.update(timeElapsedS);
+    this.fpsCamera_.update(timeElapsedS);
   }
 }
 
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new CharacterControllerDemo();
+  _APP = new FirstPersonCameraDemo();
 });
 // class BasicCharacterController {
 //   constructor(params) {
